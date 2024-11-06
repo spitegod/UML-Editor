ce68f10bfd6d03237221efcd5c5eb20a8aa5b20c
spitegod <iermakov2207@gmail.com>
2024-11-06 09:05:27 +0300

    ОТ ПАВЛА: внедрено окно редактирования

--- UMLEditor/umleditor.py
+++ UMLEditor/umleditor.py
@@ -4,8 +4,9 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from Static import Ui_StaticWidget  # Импортируем класс Ui_StaticWidget
 from PyQt5.QtCore import QTimer, QTime, QDateTime
 from PyQt5.QtCore import pyqtSignal, QByteArray, QDataStream, QIODevice, QPoint, Qt, QMimeData  # Импортируем pyqtSignal
-from PyQt5.QtWidgets import QApplication, QFrame, QHBoxLayout, QLabel, QWidget, QGridLayout
+from PyQt5.QtWidgets import QApplication, QFrame, QHBoxLayout, QLabel, QWidget, QGridLayout, QDialog
 from PyQt5.QtGui import QPixmap, QDrag
+
 
 
 #НАЧАЛО ПЕРЕДЕЛЫВАНИЯ ПОД QFRAME
@@ -47,6 +48,7 @@ class ToolbarWidget(QFrame):
             else:
                 print(f"Изображение {icon_path} не найдено")
     
+
     def mousePressEvent(self, event):
         child = self.childAt(event.pos())
         if not child:
@@ -167,6 +169,94 @@ class UserManager:
                 return user
         raise ValueError(f"Пользователя с id: {_id} нет!")
 
+
+#--------------------
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(139, 136)
+        self.gridLayout_2 = QtWidgets.QGridLayout(Dialog)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setObjectName("gridLayout")
+        self.label_4 = QtWidgets.QLabel(Dialog)
+        self.label_4.setObjectName("label_4")
+        self.gridLayout.addWidget(self.label_4, 0, 0, 1, 1)
+        self.formLayout = QtWidgets.QFormLayout()
+        self.formLayout.setObjectName("formLayout")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setObjectName("label")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label)
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setObjectName("label_2")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.label_2)
+        self.pushButton_BGcolor = QtWidgets.QPushButton(Dialog)
+        self.pushButton_BGcolor.setStyleSheet("background-color: rgb(255, 0, 0);")
+        self.pushButton_BGcolor.setText("")
+        self.pushButton_BGcolor.setObjectName("pushButton_BGcolor")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.pushButton_BGcolor)
+        self.lineEdit_text = QtWidgets.QLineEdit(Dialog)#<---------------------------------------------------
+        self.lineEdit_text.setText("")
+        self.lineEdit_text.setObjectName("lineEdit_text")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.lineEdit_text)
+        self.gridLayout.addLayout(self.formLayout, 1, 0, 1, 1)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.pushButton_delete = QtWidgets.QPushButton(Dialog)
+        self.pushButton_delete.setStyleSheet("color: rgb(255, 0, 0);")
+        self.pushButton_delete.setObjectName("pushButton_delete")
+        self.horizontalLayout_2.addWidget(self.pushButton_delete)
+        self.line = QtWidgets.QFrame(Dialog)
+        self.line.setStyleSheet("background-color: rgb(117, 117, 117);")
+        self.line.setFrameShape(QtWidgets.QFrame.VLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.horizontalLayout_2.addWidget(self.line)
+        self.pushButton_copy = QtWidgets.QPushButton(Dialog)
+        self.pushButton_copy.setObjectName("pushButton_copy")
+        self.horizontalLayout_2.addWidget(self.pushButton_copy)
+        self.gridLayout.addLayout(self.horizontalLayout_2, 4, 0, 1, 1)
+        self.line_2 = QtWidgets.QFrame(Dialog)
+        self.line_2.setStyleSheet("background-color: rgb(117, 117, 117);")
+        self.line_2.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line_2.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_2.setObjectName("line_2")
+        self.gridLayout.addWidget(self.line_2, 3, 0, 1, 1)
+        self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
+
+        self.retranslateUi(Dialog)
+        
+        # Подключение сигнала нажатия кнопки к слоту
+        self.pushButton_BGcolor.clicked.connect(self.open_color_dialog)
+
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Управление"))
+        self.label_4.setText(_translate("Dialog", "Управление"))
+        self.label.setText(_translate("Dialog", "Цвет"))
+        self.label_2.setText(_translate("Dialog", "Tекст"))
+        self.pushButton_delete.setText(_translate("Dialog", "Удалить"))
+        self.pushButton_copy.setText(_translate("Dialog", "Копировать"))
+
+    def open_color_dialog(self):
+        # Открытие диалогового окна выбора цвета
+        color = QtWidgets.QColorDialog.getColor()
+
+        # Если цвет выбран, изменить цвет кнопки
+        if color.isValid():
+            self.pushButton_BGcolor.setStyleSheet(f"background-color: {color.name()};")
+            self.lineEdit_text.setStyleSheet(f"QLineEdit {{ color: {color.name()} }}")#цвет текста
+#----------------------------------
+
+# Класс для создания диалогового окна, наследуемый от QDialog
+class DialogWindow(QtWidgets.QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Dialog()  # Экземпляр класса интерфейса
+        self.ui.setupUi(self)  # Настройка интерфейса в окне
+
 class Ui_MainWindow(QtWidgets.QMainWindow):
     time_updated = pyqtSignal(str, str, str)  # Создаем сигнал с параметром типа str для передачи запущенного времени
     update_last_timeSW = pyqtSignal(str, str, str)  # Создаем сигнал для передачи последнего значения времени
@@ -176,6 +266,7 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
     # def __init__(self):
     #     super().__init__()
 
+    
 
     def setupUi(self, MainWindow):
 
@@ -326,6 +417,7 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.start()
 
 
+
         #self.Start_Time.textChanged.connect(self.update_time)
 
         self.action_time_start.triggered.connect(self.start)
@@ -336,7 +428,19 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    #Ниже 7 функции - реализация работы таймера
+
+
+    # Подключаем обработчик событий для ПКМ ---------------------------------------------------------------------------
+        self.workspace.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.workspace.customContextMenuRequested.connect(self.open_dialog)
+
+    def open_dialog(self):
+        # Создаем и отображаем диалоговое окно
+        dialog = DialogWindow()
+        dialog.exec_()
+        #------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        #Ниже 7 функции - реализация работы таймера
 
     def start(self):
         if not self.running:  # Запускаем таймер, только если он не запущен
@@ -472,6 +576,8 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.action_time_start.setText(_translate("MainWindow", "Запустить таймер"))
         self.action_time_stop.setText(_translate("MainWindow", "Остановить таймер"))
         self.action_time_reset.setText(_translate("MainWindow", "Сбросить таймер"))
+
+
 
 
 if __name__ == "__main__":
